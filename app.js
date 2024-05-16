f6088475ca0bcd201e9ee36636362ed96b4de818
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-05-16 22:03:59 +0200

    started thursday

---
+++ app.js
@@ -0,0 +1,7 @@
+//
+
+
+player1Name = document.querySelector(".first-place .player-name");
+console.log(player1Name);
+player2Name = document.querySelector(".player-name");
+player3Name = document.querySelector(".player-name");
